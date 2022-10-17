80d6bc236bbd7fd6f3a93f752924673885083b42
dPaulroses(315) <109587195+dpaulroses@users.noreply.github.com>
2022-10-17 16:43:57 +0600

    Create Team.cpp

---
+++ Lab-2/Team.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,p,v,t,count=0;
+cin>>n;
+for(i=0;i<n;i++)
+{
+cin>>p>>v>>t;
+if(p+v+t>=2)
+ {
+   count++;
+ }
+}
+cout<<count<<endl;
+return 0;
+}
